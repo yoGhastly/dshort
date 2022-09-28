583282964ec2758eda4e248e2a2d6eb2854cf1d9
yoGhastly <ve06976@innovaccion.mx>
2022-09-27 20:01:57 -0500

    actual redirect at root

---
+++ src/middleware.ts
@@ -0,0 +1,27 @@
+import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
+import { nanoid } from "nanoid";
+
+export async function middleware(req: NextRequest, ev: NextFetchEvent) {
+  if (req.nextUrl.pathname.startsWith("/api/get-url/")) {
+    console.log("returning early");
+    return;
+  }
+
+  console.log('Path??', req.nextUrl.pathname)
+
+  const slug = req.nextUrl.pathname.split("/").pop()
+
+  console.log('Full nextUrl', req.nextUrl)
+
+  const data = await (
+    await fetch(`${req.nextUrl.origin}/api/get-url/${slug}`)
+  ).json()
+
+  if (data?.url) {
+    return NextResponse.redirect(data.url)
+  }
+
+  console.log("data", data);
+
+}
+
